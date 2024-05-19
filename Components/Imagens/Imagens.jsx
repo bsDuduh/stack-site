598f9b023a310cf55769e38152969e70a60bd388
Davi Varella <davivarelladev@gmail.com>
2024-05-19 04:00:30 -0300

    conteudo landing page pt 1

--- Components/Imagens/Imagens.jsx
+++ Components/Imagens/Imagens.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import '../../Style/Imagens/Imagens.css'
 import logoEscrito from '../../src/assets/logo_escrita.png'
-export default function Imagens({ src, alt, className}){
+import seta from '../../src/assets/seta.png'
+export default function Imagens({ src, alt, className }) {
 
-    return(
+    return (
         <div className="container">
-            <img src={src} alt={alt} className={className}/>
+            <img src={src} alt={alt} className={className} />
             <div className="Logo">
-                <img src={logoEscrito} alt="logo escrito stack"/>
+                <img src={logoEscrito} alt="logo escrito stack" />
+                <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+                <div className="seta">
+                    <a href=""><img src={seta} alt="seta" /></a>
+                </div>
+
             </div>
         </div>
     );
